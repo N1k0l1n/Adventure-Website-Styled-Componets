d6dc59d8216915aee58da4d0bf29303eff3c4d2f
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-09-26 16:26:25 +0200

    bugg in video background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import './App.css';
-import Home from './components/Home';
+import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
