23955b55f335d62f199a84d9a525d2589cbc9268
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-09-26 15:51:40 +0200

    First View

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
