4474278d4c0d967cf8d5bf357a67a7f8223e9b65
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-09-27 13:54:41 +0200

    First Video View

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact component={Home} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Outlet />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,7 +6,7 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="hero-container">
-      <video src="../../../public/videos/video-2.mp4" autoPlay loop muted />
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
